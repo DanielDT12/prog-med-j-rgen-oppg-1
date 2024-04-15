31e20b93a63da33c66f38d1a5e81cba3929a4263
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-04-15 15:07:56 +0200

    Oppgave 1 programering med jørgen

---
+++ oppgave-1.py
@@ -0,0 +1,15 @@
+# oppgave 1
+
+myString = "This is a test" # Start string
+myString += " " # myString = myString + " "
+myString += "string" # Legger til "string" etter at det er lagt til " "
+
+print(myString)
+
+x = 4
+y = 10
+z = y
+
+x -= y # x = x - y === -6
+
+print(x)
